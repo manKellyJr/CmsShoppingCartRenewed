c00848b0678f2f3063caa2e7350e5016d762c6bf
Obed Banda <obedbanda34@gmail.com>
2022-11-18 15:58:51 +0200

    Add project files.

---
+++ CmsShoppingCart/Infrastructure/CmsShoppingCartContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public class CmsShoppingCartContext : DbContext
+    {
+        public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options) : base(options)
+        {
+
+        }
+    }
+}
